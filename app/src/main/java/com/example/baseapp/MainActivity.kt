03501a37fc9f85174aea3eb5ab50265d4f931c3d
paveldolgov4 <dolgoff1272@gmail.com>
2022-04-13 12:57:40 +0300

    LiveData

--- app/src/main/java/com/example/baseapp/MainActivity.kt
+++ app/src/main/java/com/example/baseapp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.baseapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.baseapp.databinding.ActivityMainBinding
+import com.example.baseapp.SimpleViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    // Obtain ViewModel from ViewModelProviders
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding : ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.name = "Pavel"
+        binding.lifecycleOwner = this
+
+        binding.viewmodel = viewModel
     }
 }
